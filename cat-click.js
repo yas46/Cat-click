be35800b234b378fa0e473167420ca0fd3e86300
Yasser Beyer <yas2pt0@gmail.com>
2017-04-26 21:28:54 -0400

    final update

--- cat-click.js
+++ cat-click.js
@@ -1,27 +1,32 @@
-//This JS file only has two cats - also, has no cat menu.
 
-var cat1 = document.getElementById('cat-photo1');
-var cat2 = document.getElementById('cat-photo2');
-var clicks1 = document.getElementById('counter1');
-var clicks2 = document.getElementById('counter2');
-var catName1 = document.getElementById('cat1');
-var catName2 = document.getElementById('cat2');
+var catName1 = document.getElementById('cat-name');
+//var catName2 = document.getElementById('cat-name2');
+var clicks1 = document.getElementById('click-count');
+//var clicks2 = document.getElementById('click-count2');
+var cat1Photo = document.getElementById('cat-photo');
+//var cat2Photo = document.getElementById('cat-photo2');
 var count1 = 0;
-var count2 = 0;
+//var count2 = 0;
 var name1 = "Bob";
-var name2 = "Felix";
+//var name2 = "Felix";
 
 
-catName1.innerHTML = name1;
-catName2.innerHTML = name2;
+catName1.textContent = name1;
+//catName2.textContent = name2;
+
+clicks1.textContent = count1;
+//catName2.textContent = count2;
+
+cat1Photo.src = "https://c2.staticflickr.com/2/1557/25135549306_b671c01053_z.jpg";
+//cat2Photo.src = "https://c2.staticflickr.com/2/1557/25135549306_b671c01053_z.jpg";
 
 
-cat1.addEventListener('click', function() {
+cat1Photo.addEventListener('click', function() {
     count1++;
-    clicks1.innerHTML = count1;
+    clicks1.textContent = count1;
 });
 
-cat2.addEventListener('click', function() {
-    count2++;
-    clicks2.innerHTML = count2;
-});
+// cat2Photo.addEventListener('click', function() {
+//     count2++;
+//     clicks2.textContent = count2;
+// });
