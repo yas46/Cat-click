25bbe8f4c6fab2504494500e5f18492407bb4b5b
Yasser Beyer <yas2pt0@gmail.com>
2017-04-26 22:35:47 -0400

    created the model

--- cat-click-MVC.js
+++ cat-click-MVC.js
@@ -1,22 +1,41 @@
 $(function(){
 
-    var listView = {
-        init: function(name) {
-            var ul = document.getElementById('cat-list');
-            var li = document.createElement("li");
-            listView.render(name)
-        },
-        render: function(name) {
-            li.appendChild(document.createTextNode(name));
-            li.addEventListener('click', (function() {
-                return function() {
-                    hideCat();
-                    document.getElementById(name).style.display = 'block';
-                }
-            })());
-            ul.appendChild(li);
-        }
+    var model = {
+        currentCat: null,
+        catList: [
+            {
+                clickCount: 0,
+                name: "Tom",
+                url: "c2.staticflickr.com/2/1557/25135549306_b671c01053_z"
+            },
+            {
+                clickCount: 0,
+                name: "Felix",
+                url: "c2.staticflickr.com/6/5482/11637643254_8d7d8f4b48_z"
+            },
+            {
+                clickCount: 0,
+                name: "Garfield",
+                url: "c1.staticflickr.com/9/8501/8300920648_d4a21bba59"
+            },
+            {
+                clickCount: 0,
+                name: "Alley",
+                url: "c1.staticflickr.com/3/2386/1696417701_2ba5cda00c"
+            },
+            {
+                clickCount: 0,
+                name: "Sue",
+                url: "c2.staticflickr.com/4/3360/3591850431_4599f509e2"
+            },
+        ]
     };
+
+
+    var controller = {
+
+    };
+
 
     var catView = {
         init: function(name, url) {
@@ -50,12 +69,23 @@ $(function(){
 
     };
 
-    var octopus = {
-
+    var listView = {
+        init: function(name) {
+            var ul = document.getElementById('cat-list');
+            var li = document.createElement("li");
+            listView.render(name)
+        },
+        render: function(name) {
+            li.appendChild(document.createTextNode(name));
+            li.addEventListener('click', (function() {
+                return function() {
+                    hideCat();
+                    document.getElementById(name).style.display = 'block';
+                }
+            })());
+            ul.appendChild(li);
+        }
     };
 
-    var model = {
-        var catList = [];
-    };
 
 });
